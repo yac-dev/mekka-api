2f4fc0d39c5e76570d587864b9a2584fb1156359
kojima yosuke <yabbee0828@gmail.com>
2023-11-23 11:57:02 +0900

    revert

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-// import './databases/mongoose';
+import './databases/mongoose';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
