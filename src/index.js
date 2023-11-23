a74789c0b871eede62ed84d8b9f47fe09d15de30
kojima yosuke <yabbee0828@gmail.com>
2023-11-23 11:14:42 +0900

    Mongo...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import http from 'http';
 import app from './app';
+import mongoose from 'mongoose';
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3500, () => {
-  console.log('listening on port 3500');
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected👍`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDB().then(() => {
+  app.listen(3500, () => {
+    console.log('listening for requests');
+  });
 });
+
+// server.listen(3500, () => {
+//   console.log('listening on port 3500');
+// });
