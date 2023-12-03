c424a088ae0564d43d680ad1c2893e1ed8e46e54
kojima yosuke <yabbee0828@gmail.com>
2023-12-03 12:05:55 +0900

    Fix posts handler

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import app from './app.js';
 const port = process.env.PORT || 3500;
 const server = http.createServer(app);
 
+// dynamic portにしないとherokuで動かねー。まじ詰まったわ。。。。
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
