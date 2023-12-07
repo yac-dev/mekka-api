ab24477f1f936f4a086a6c92538ed782b8541b4b
kojima yosuke <yabbee0828@gmail.com>
2023-12-07 20:02:29 +0900

    Remove comment

--- src/models/post.js
+++ src/models/post.js
@@ -3,14 +3,6 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({
   contents: [{ type: mongoose.Schema.ObjectId, ref: 'Content' }],
   caption: String,
-  // location: {
-  //   type: {
-  //     type: String,
-  //     enum: ['Point'],
-  //     default: 'Point',
-  //   },
-  //   coordinates: [Number],
-  // },
   locationTag: { type: mongoose.Schema.ObjectId, ref: 'LocationTag' }, // これは一つのみ。
   space: { type: mongoose.Schema.ObjectId, ref: 'Space' },
   createdBy: { type: mongoose.Schema.ObjectId, ref: 'User' },
