85dc928a0cb59151998f296b805740859ef26071
kojima yosuke <yabbee0828@gmail.com>
2024-02-08 19:01:35 +0900

    Fix import statement

--- src/routers/icons.js
+++ src/routers/icons.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getIconByName } from '../controllers/icons';
+import { getIconByName } from '../controllers/icons.js';
 
 router.route('/').get(getIconByName);
 
