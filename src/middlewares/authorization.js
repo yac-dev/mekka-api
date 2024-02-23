22b2406d2052b77b06061f138972edcf7783ae7e
kojima yosuke <yabbee0828@gmail.com>
2024-02-24 02:20:11 +0900

    Fix authorization

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,28 +1,26 @@
 import User from '../models/user.js';
 import jwt from 'jsonwebtoken';
+import { AppError } from '../utils/AppError.js';
 
 export const authorization = async (request, response, next) => {
-  try {
-    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer')) {
-      let token = request.headers.authorization.split(' ')[1];
-      if (!token) {
-        console.log('Can not authorize because of no token');
-      }
-      const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY); // decoded {id: _id}っていう形。
-      const user = await User.findById(decoded.id).select({
-        _id: true,
-        name: true,
-        email: true,
-        pushToken: true,
-        avatar: true,
-      });
-      if (!user) {
-        throw new Error('Cant find that user');
-      }
-      request.user = user;
-      next();
-    }
-  } catch (error) {
-    console.log(error);
+  let token;
+  if (request.headers.authorization && request.headers.authorization.startsWith('Bearer')) {
+    token = request.headers.authorization.split(' ')[1];
   }
+  if (!token) {
+    return next(new AppError('The token was not provided.', 401));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY); // decoded {id: _id}っていう形。
+  const user = await User.findById(decoded.id).select({
+    _id: true,
+    name: true,
+    email: true,
+    pushToken: true,
+    avatar: true,
+  });
+  if (!user) {
+    return next(new AppError('The user with this token does no longer exist.', 401));
+  }
+  request.user = user;
+  next();
 };
