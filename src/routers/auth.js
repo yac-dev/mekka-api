8faea858c352b9702ca500da49d87a551b3cf746
kojima yosuke <yabbee0828@gmail.com>
2024-03-05 22:27:23 +0900

    add handlers

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,20 @@
 import express from 'express';
 const router = express.Router();
-import { signup, loadMe, login, deleteMe, registerPushToken, forgotPassword } from '../controllers/auth.js';
+import {
+  signup,
+  loadMe,
+  login,
+  deleteMe,
+  registerPushToken,
+  forgotPassword,
+  checkPINcode,
+} from '../controllers/auth.js';
 import { authorization } from '../middlewares/authorization.js';
 
 router.route('/signup').post(signup);
 router.route('/login').post(login);
 router.route('/forgotPassword').post(forgotPassword);
+router.route('/checkpin').post(checkPINcode);
 router.get('/loadme', authorization, loadMe);
 router.route('/').delete(deleteMe);
 router.route('/:userId/pushToken').patch(registerPushToken);
