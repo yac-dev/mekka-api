ac1a8e521cd733b02d7ff835ac5160b0eb50cc8f
kojima yosuke <yabbee0828@gmail.com>
2024-08-08 00:53:09 +0900

    Add notification routing

--- src/routers/notifications.js
+++ src/routers/notifications.js
@@ -1,4 +1,7 @@
 import express from 'express';
 const router = express.Router();
+import { createPostNotification } from '../controllers/notifications.js';
+
+router.route('/').get(createPostNotification);
 
 export default router;
