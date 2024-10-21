32793bfc978b1afe7698bc203ef98ec2aa8ed8b6
kojima yosuke <yabbee0828@gmail.com>
2024-10-21 23:49:48 +0900

    Change defult tag color

---
+++ src/utils/colorOptions.js
@@ -0,0 +1,13 @@
+export const colorOptios = [
+  'red1',
+  'yellow1',
+  'orange1',
+  'blue1',
+  'violet1',
+  'lightGreen1',
+  'green1',
+  'lightBlue1',
+  'pink1',
+  'black1',
+  'brown1',
+];
