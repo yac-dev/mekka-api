0ab1cbf7af21e79991cddfed534aacc9f2e37a96
kojima yosuke <yabbee0828@gmail.com>
2025-02-21 20:35:25 +0900

    Add following Relation ship handler

--- src/controllers/followingRelationships.js
+++ src/controllers/followingRelationships.js
@@ -2,13 +2,15 @@ import FollowingRelationship from '../models/followingRelationship.js';
 
 export const createFollowingRelationship = async (request, response) => {
   try {
-    const { follower, followee, spaceId } = request.body;
-    const followingRelationship = await FollowingRelationship.create({ follower, followee, spaceId });
+    const { followerId, followeeId, spaceId } = request.body;
+    const followingRelationship = await FollowingRelationship.create({
+      follower: followerId,
+      followee: followeeId,
+      space: spaceId,
+    });
     response.status(201).json({
       data: {
-        follower,
-        followee,
-        spaceId,
+        followingRelationship,
       },
     });
   } catch (error) {
@@ -19,12 +21,59 @@ export const createFollowingRelationship = async (request, response) => {
   }
 };
 
-export const getFollowingRelationship = async (request, response) => {
+// あるユーザーがfollowしているユーザーリストを返す。
+export const getFollowingUsersByUserId = async (request, response) => {
   try {
-    const followingRelationship = await FollowingRelationship.find({});
+    const { userId } = request.params;
+    const followingRelationships = await FollowingRelationship.find({ follower: userId }).populate({
+      path: 'followee',
+      model: 'User',
+    });
+
+    const usersBySpace = followingRelationships.reduce((acc, followingRelationship) => {
+      const spaceId = followingRelationship.space.toString();
+      const followee = followingRelationship.followee;
+
+      if (followee) {
+        if (!acc[spaceId]) {
+          acc[spaceId] = [];
+        }
+        acc[spaceId].push({
+          id: followee._id,
+          name: followee.name,
+          email: followee.email,
+          avatar: followee.avatar,
+        });
+      }
+
+      return acc;
+    }, {});
+
     response.status(200).json({
-      success: true,
-      followingRelationship,
+      data: {
+        followingUsers: usersBySpace,
+      },
+    });
+  } catch (error) {
+    response.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const deleteFollowingRelationship = async (request, response) => {
+  try {
+    const { followerId, followeeId, spaceId } = request.body;
+    const followingRelationship = await FollowingRelationship.findOneAndDelete({
+      follower: followerId,
+      followee: followeeId,
+      space: spaceId,
+    });
+    response.status(200).json({
+      data: {
+        followingRelationship,
+      },
     });
   } catch (error) {
     response.status(500).json({
