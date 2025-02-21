ee13e3b4553e3981f08d3428e86f2cb522e7977b
kojima yosuke <yabbee0828@gmail.com>
2025-02-21 20:35:25 +0900

    Add following Relation ship handler

--- src/routers/followingRelationships.js
+++ src/routers/followingRelationships.js
@@ -1,7 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { getFollowingRelationship, createFollowingRelationship } from '../controllers/followingRelationships.js';
+import {
+  createFollowingRelationship,
+  getFollowingUsersByUserId,
+  deleteFollowingRelationship,
+} from '../controllers/followingRelationships.js';
 
 router.route('/').post(createFollowingRelationship);
-
+router.route('/').delete(deleteFollowingRelationship);
+router.route('/users/:userId').get(getFollowingUsersByUserId);
 export default router;
