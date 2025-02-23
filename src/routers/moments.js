2fd20225940d2d03a1a8d909c234483b1e1b45da
kojima yosuke <yabbee0828@gmail.com>
2025-02-23 23:29:18 +0900

    Add create moment for moment resource

--- src/routers/moments.js
+++ src/routers/moments.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import multerParser from '../middlewares/multer.js';
 import { createMoment, getMomentsBySpaceId } from '../controllers/moments.js';
 
-router.route('/').post(multerParser.array('contents', 10), createMoment);
+router.route('/').post(multerParser.array('bufferContents', 10), createMoment);
 // router.route('/:momentoId').get(getPost);
 router.route('/:spaceId').get(getMomentsBySpaceId);
 
