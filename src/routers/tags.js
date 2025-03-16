bd5cb645782bd77a881d225556921ad7ebcf7b03
kojima yosuke <yabbee0828@gmail.com>
2025-03-16 21:49:02 +0900

    Fix routing for getPostsByTagId

--- src/routers/tags.js
+++ src/routers/tags.js
@@ -1,4 +1,7 @@
 import express from 'express';
 const router = express.Router();
+import { getPostsByTagId } from '../controllers/tags.js';
+
+router.route('/:tagId/posts').get(getPostsByTagId);
 
 export default router;
