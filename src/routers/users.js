0782d4cd49469a8544d98f5d0bf7b91c8ea6db98
kojima yosuke <yabbee0828@gmail.com>
2025-03-17 20:58:53 +0900

    Add aggregation to getSpacesByUserId, getPostsBySpaceId

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { getUsersBySpaceId, getUsersByAddress, getUserById } from '../controllers/users.js';
+import { getUsersBySpaceId, getUsersByAddress, getUserById, getSpacesByUserId } from '../controllers/users.js';
 import { updateSpaceLastCheckedIn } from '../controllers/spaceAndUserRelationships.js';
 
+router.route('/:userId/spaces').get(getSpacesByUserId);
 router.route('/:spaceId/space').get(getUsersBySpaceId);
 router.route('/:userId/lastcheckedin').patch(updateSpaceLastCheckedIn);
 router.route('/:userId').get(getUserById);
